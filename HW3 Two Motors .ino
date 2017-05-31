50ed5ac47cdbc44beee5d993a3fd90e4a8ee83e1
melissa1126 <yyin@cca.edu>
2017-05-31 00:13:09 -0700

    Create HW3 Two Motors .ino

---
+++ HW3 Two Motors .ino
@@ -0,0 +1,17 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(6, OUTPUT);
+ pinMode(5, OUTPUT);
+ pinMode(3, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ digitalWrite(2, HIGH);
+  digitalWrite(4, LOW);
+  digitalWrite(6, HIGH);
+    digitalWrite(5, HIGH);
+      digitalWrite(3, LOW);
+}
